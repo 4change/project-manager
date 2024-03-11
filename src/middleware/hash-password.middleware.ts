bfaff3f50ebe56654f2d4e5c594e2acc12b9565b
4change <e_mail4gxf@163.com>
2024-03-11 10:43:24 +0800

    10-2.使用 crypto 对密码进行安全加密

---
+++ src/middleware/hash-password.middleware.ts
@@ -0,0 +1,16 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { NextFunction } from 'express';
+import { addSalt, encript } from 'src/utils/encription';
+
+@Injectable()
+export class HashPasswordMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: NextFunction) {
+    let userPassword = req.body['password']
+    if (userPassword) {
+      userPassword = encript(userPassword, addSalt())
+      req.body['password'] = userPassword
+    }
+    console.log(req.body['password'])
+    next();
+  }
+}
--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,14 +1,13 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
-import { HelloMiddleware } from 'src/middleware/hello.middleware';
-
+import { HashPasswordMiddleware } from 'src/middleware/hash-password.middleware';
 @Module({
   providers: [UserService],
   controllers: [UserController]
 })
 export class UserModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-      consumer.apply(HelloMiddleware).forRoutes('user')
+    consumer.apply(HashPasswordMiddleware).forRoutes('user')
   }
 }
